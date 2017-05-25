7ec37fd141b841e913881284b7fcdc64c0f7909a
sur1910 <onenight1910@gmail.com>
2017-05-26 08:41:36 +0900

    first commit

---
+++ Serch-Build-bot/README.md
@@ -0,0 +1,9 @@
+# Search-Build-Bot
+
+> 'Serch-Build-Bot'は[champion.gg](http://champion.gg)から各チャンピオンの高頻度で使われるビルドを表示します。
+
+## Instructions
+
+- [ ] config-sample.pyをconfig.pyに変更してください。
+- [ ] config.pyのTOKEN_IDを自分のものに書き直してください。
+- [ ] python search-build-bot.py
---
+++ Serch-Build-bot/search-build-bot.py
@@ -0,0 +1,55 @@
+import json
+import requests
+from lxml import html
+import discord
+import logging
+import urllib
+import config
+from bs4 import BeautifulSoup
+
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('起動しました!')
+    print('ユーザーネーム: {}  ユーザーID: {}'.format(client.user.name, client.user.id))
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!build') and client.user.id != message.author.id:
+        try:
+            champion, role = message.content.split()[1:]
+        except:
+            await client.send_message(message.channel, '!build [champion] [top|jungle|middle|adc|support]')
+        else:
+            champion = champion[0].upper() + champion[1:].lower()
+            role = role[0].upper() + role[1:].lower()
+            try:
+                item_build = fetch_build(champion, role)
+            except:
+                await client.send_message(message.channel, 'Unknown build combination or error fetching request.')
+            else:
+                await client.send_message(message.channel, item_build)
+                print("投稿者：", message.author, "メッセージ：", message.content)
+    elif message.content.startswith('!ping'):
+        await client.send_message(message.channel, 'pong')
+
+def fetch_build(champion, role):
+    try:
+        r = 'http://champion.gg/champion/{}/{}'.format(champion, role)
+    except requests.exceptions.HTTPError:
+        return None
+    else:
+        html = urllib.request.urlopen(r)
+        soup = BeautifulSoup(html, "lxml")
+        tables = soup.findAll('div', class_='build-wrapper')
+        a = tables[0].findAll('a')
+        item_build = []
+        for i in a:
+            item_build.append(i.attrs['href'].split('/')[-1])
+
+        return item_build
+
+client.run(config.TOKEN_ID)
